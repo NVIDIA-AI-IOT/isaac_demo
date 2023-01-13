7dfd2e9023b2586f6f3f41379411cd057c350e8a
Raffaello Bonghi <raffaello@rnext.it>
2023-01-13 19:10:15 +0000

    Renaming

---
+++ isaac_ros/src/isaac_demo/launch/carter.launch.py
@@ -0,0 +1,99 @@
+# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import os
+import launch
+from launch_ros.actions import ComposableNodeContainer, Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.descriptions import ComposableNode
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+
+
+def generate_launch_description():
+    
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+
+    use_sim_dec = DeclareLaunchArgument(
+        'use_sim_time', default_value='True',
+        description='Use simulation (Omniverse Isaac Sim) clock if true')
+
+    visual_slam_node = ComposableNode(
+        name='visual_slam_node',
+        package='isaac_ros_visual_slam',
+        plugin='isaac_ros::visual_slam::VisualSlamNode',
+        remappings=[('stereo_camera/left/camera_info', '/left/camera_info'),
+                    ('stereo_camera/right/camera_info', '/right/camera_info'),
+                    ('stereo_camera/left/image', '/left/rgb'),
+                    ('stereo_camera/right/image', '/right/rgb')],
+        parameters=[{
+            'enable_rectified_pose': True,
+            'denoise_input_images': True,
+            'rectified_images': True,
+            'enable_debug_mode': False,
+            'debug_dump_path': '/tmp/elbrus',
+            'left_camera_frame': 'carter_camera_stereo_left',
+            'right_camera_frame': 'carter_camera_stereo_right',
+            'map_frame': 'map',
+            'fixed_frame': 'odom',
+            'odom_frame': 'odom',
+            'base_frame': 'base_link',
+            'current_smooth_frame': 'base_link_smooth',
+            'current_rectified_frame': 'base_link_rectified',
+            'enable_observations_view': True,
+            'enable_landmarks_view': True,
+            'enable_reading_slam_internals': True,
+            'enable_slam_visualization': True,
+            'enable_localization_n_mapping': True,
+            'use_sim_time': use_sim_time
+        }]
+    )
+    visual_slam_launch_container = ComposableNodeContainer(
+        name='visual_slam_launch_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            visual_slam_node
+        ],
+        output='screen'
+    )
+
+    # https://foxglove.dev/docs/studio/connection/ros2
+    # https://github.com/foxglove/ros-foxglove-bridge
+    foxglove_bridge_node = Node(
+        package='foxglove_bridge',
+        executable='foxglove_bridge',
+        output='screen'
+    )
+
+    # Launch ROS2 packages
+    ld = LaunchDescription()
+
+    ld.add_action(use_sim_dec)
+    ld.add_action(visual_slam_launch_container)
+    ld.add_action(foxglove_bridge_node)
+
+    return ld
+# EOF
